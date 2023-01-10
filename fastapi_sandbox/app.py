086b3c13bbc0087815a93b93fdbc5e241c7844eb
Алексей Головизин <agolovizin@alfabank.ru>
2023-01-10 18:01:01 +0300

    minimal example...

--- fastapi_sandbox/app.py
+++ fastapi_sandbox/app.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, Header
 from fastapi.responses import RedirectResponse
 from fastapi_keycloak import FastAPIKeycloak, OIDCUser
 
@@ -35,10 +35,41 @@ def login_redirect():
     return RedirectResponse(idp.login_uri)
 
 
+@app.get("/ask_to_login_1")
+def ask_to_login_1(authorization: str = Header(default=None)):
+
+    # Если у пользователя нет токена, или токен протух -- посылаем его логиниться.
+    if authorization is None:
+        return RedirectResponse(idp.login_uri)
+    assert authorization.startswith("Bearer ")
+    token = authorization[len("Bearer "):]
+    if not idp.token_is_valid(token):
+        return RedirectResponse(idp.login_uri)
+
+    return "RESULT FROM METHOD 1"
+
+
+@app.get("/ask_to_login_2")
+def ask_to_login_2(authorization: str = Header(default=None)):
+
+    # Если у пользователя нет токена, или токен протух -- посылаем его логиниться.
+    if authorization is None:
+        return RedirectResponse(idp.login_uri)
+    assert authorization.startswith("Bearer ")
+    token = authorization[len("Bearer "):]
+    if not idp.token_is_valid(token):
+        return RedirectResponse(idp.login_uri)
+
+    return "RESULT FROM METHOD 2"
+
+
 @app.get("/callback")
 def callback(session_state: str, code: str):
-    return idp.exchange_authorization_code(session_state=session_state, code=code)  # This will return an access token
+    auth_token = idp.exchange_authorization_code(session_state=session_state, code=code)  # This will return an access token
+    # Сюда пользователь будет прилетать из /ask_to_login_1 и /ask_to_login_2,
+    # Но как мне понять, из какого метода сюда прилетел юзер, чтобы вернуть ему разный результат?
 
+    return auth_token
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8081)
